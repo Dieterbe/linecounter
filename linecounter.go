be42274203288286839a485a99988c2812374049
Dieter Plaetinck <dieter@plaetinck.be>
2015-09-13 18:18:36 +0300

    also print counts in the last (incomplete) data in case of error
    in particular, in case of EOF
    previously it was not accounting for data correctly, see cases like:
    (echo -e 'a\nb\nc'; sleep 1.2; echo -e 'a\nb') | ./linecounter
    (echo -e 'a\nb\nc'; sleep 1.2; echo -e 'a\nb\n') | ./linecounter
    (echo -e 'a\nb\nc'; sleep 1.2; echo -e 'a\nb\nc') | ./linecounter

--- linecounter.go
+++ linecounter.go
@@ -10,44 +10,48 @@ import (
 )
 
 var freq int
-var data chan int
 
 func init() {
 	flag.IntVar(&freq, "freq", 1000, "report frequency in ms")
-	data = make(chan int)
 }
 
 func main() {
-    flag.Parse()
-	go tracker()
-	reader()
-}
-
-func reader() {
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		_, err := reader.ReadString('\n')
-		if err == io.EOF {
-			return
-		}
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err.Error())
-			os.Exit(2)
-		}
-		data <- 1
-	}
+	flag.Parse()
+	tracker()
 }
 
 func tracker() {
+	data := make(chan int)
+	exit := make(chan int)
+	reader := bufio.NewReader(os.Stdin)
+	go func() {
+		for {
+			l, err := reader.ReadString('\n')
+			if err == io.EOF {
+				if len(l) > 0 {
+					data <- 1
+				}
+				exit <- 0
+			}
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err.Error())
+				exit <- 2
+			}
+			data <- 1
+		}
+	}()
 	counter := 0
 	tick := time.NewTicker(time.Duration(freq) * time.Millisecond)
-    for {
-        select {
-        case <-tick.C:
-            fmt.Println(counter)
-            counter = 0
-        case diff := <-data:
-            counter += diff
-        }
-    }
+	for {
+		select {
+		case <-tick.C:
+			fmt.Println(counter)
+			counter = 0
+		case ret := <-exit:
+			fmt.Println(counter, "(incomplete interval)")
+			os.Exit(ret)
+		case diff := <-data:
+			counter += diff
+		}
+	}
 }
